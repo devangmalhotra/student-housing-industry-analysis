1c593efa81f2c90194cee4be1fa9442469f09751
devangmalhotra <malh2226@mylaurier.ca>
2025-04-01 21:28:21 -0400

    updated files

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,8 +3,12 @@ import { Link } from 'react-router-dom';
 import infoIcon from './assets/info-circle-svgrepo-com.svg';
 import darkModeIcon from './assets/moon-svgrepo-com.svg';
 import { NavLink } from 'react-router-dom';
+import { FiMenu } from "react-icons/fi";
+import { useState } from 'react';
 
 function Navbar() {
+  
+
   return (
     <nav id='navbar'>
       <Link to='/'><h3>Student Housing Industry Analysis</h3></Link>
@@ -12,6 +16,7 @@ function Navbar() {
         <li><img className='header-icon' src={darkModeIcon} alt="Switch modes" /><h5>Dark</h5></li>
         <li><img className='header-icon' src={infoIcon} alt="About" /><h5>About</h5></li>
       </ul>
+      <FiMenu id='hamburger'/>
     </nav>
   )
 }
