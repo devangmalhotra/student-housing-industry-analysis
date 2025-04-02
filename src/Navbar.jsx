c50ef8d4c4e071cd3603ceae4b7ddf1ae6d82316
devangmalhotra <malh2226@mylaurier.ca>
2025-04-01 21:39:51 -0400

    finished respsonsive navbar (will add dark mode and about section after project is finished)

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,16 +7,20 @@ import { FiMenu } from "react-icons/fi";
 import { useState } from 'react';
 
 function Navbar() {
-  
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleHamburger = () => {
+    setIsOpen(!isOpen);
+  } 
 
   return (
     <nav id='navbar'>
       <Link to='/'><h3>Student Housing Industry Analysis</h3></Link>
-      <ul id='header-icons'>
+      <ul id='header-icons' className={isOpen ? "" : "hidden"}>
         <li><img className='header-icon' src={darkModeIcon} alt="Switch modes" /><h5>Dark</h5></li>
         <li><img className='header-icon' src={infoIcon} alt="About" /><h5>About</h5></li>
       </ul>
-      <FiMenu id='hamburger'/>
+      <FiMenu id='hamburger' onClick={handleHamburger}/>
     </nav>
   )
 }
