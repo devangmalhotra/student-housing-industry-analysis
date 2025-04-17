4e19d20fe718c24c672a2aa3fe63514c5170a06c
devangmalhotra <malh2226@mylaurier.ca>
2025-04-17 19:24:09 -0400

    adding test data for line graph

--- src/Components/LineGraph.jsx
+++ src/Components/LineGraph.jsx
@@ -1,33 +1,12 @@
 import React from 'react';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
 
 function LineGraph(props) {
+  const lineChartData = {
+    labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+  }
 
-  
-  
   return (
-    <div>LineGraph
-       <Line  />
-    </div>
+    <div>LineGraph</div>
    
   )
 }
