a36ad2ca38e42dcf095d107ff845d0b3d75f4805
devangmalhotra <malh2226@mylaurier.ca>
2025-04-18 17:10:41 -0400

    added bar chart

--- src/Components/LineGraph.jsx
+++ src/Components/LineGraph.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import { 
   Chart as ChartJS, 
   CategoryScale, 
   LinearScale, 
   PointElement, 
-  LineElement,
+  BarElement,
   Title, 
   Tooltip, 
   Legend,
@@ -15,27 +15,45 @@ import {
     CategoryScale, 
     LinearScale, 
     PointElement, 
-    LineElement,
+    BarElement,
     Title, 
     Tooltip, 
     Legend,
  )
 
 function LineGraph(props) {
-  const options = {};
-  const data = {};
-
   const lineChartData = {
-    labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"], 
-    datasets: {
-      label: 'Steps', 
-      data: [3000, 5000, 4500, 5000, 5000], 
-      borderColor: 'rgb(5, 192, 192)', 
-    }
+    labels: [
+        "Monday", 
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday", 
+        "Friday",
+    ], 
+    datasets: [
+        {
+            label: 'Steps', 
+            data: [3000, 5000, 4500, 5000, 5000], 
+            borderColor: 'rgb(5, 192, 192)', 
+        }
+    ]
+  }; 
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
   };
 
   return (
-    <Line options={options} data={data}/>
+    <Bar data={lineChartData} options={options} />
    
   )
 }
