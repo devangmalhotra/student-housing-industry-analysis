7e39dcf0f03143876b9517fcd8d796e86bb9843d
devangmalhotra <malh2226@mylaurier.ca>
2025-04-18 15:12:44 -0400

    added imports

--- src/Components/LineGraph.jsx
+++ src/Components/LineGraph.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { 
+  Chart as ChartJS, 
+  CategoryScale, 
+  LinearScale, 
+  PointElement, 
+  LineElement,
+  Title, 
+  Tooltip, 
+  Legend,
+ } from 'chart.js';
 
 function LineGraph(props) {
   const lineChartData = {
-    labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-  }
+    labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"], 
+    datasets: {
+      label: 'Steps', 
+      data: [3000, 5000, 4500, 5000, 5000], 
+      borderColor: 'rgb(5, 192, 192)', 
+    }
+  };
 
   return (
     <div>LineGraph</div>
