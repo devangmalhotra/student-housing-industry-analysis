9046bea05322a6374b2c6ac573a6988b8fe0c994
devangmalhotra <malh2226@mylaurier.ca>
2025-04-29 15:53:58 -0400

    updated files

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import StatBox from './StatBox'
 import BarGraph from './BarGraph'
+import { useState } from 'react';
 
 function MainContent() {
+  const [selectedCity, setSelectedCity] = useState(localStorage.getItem('selected-city'));
+
   return (
     <section id='home-main-content'>
-        <StatBox statName='Total listings'/>
-        <StatBox statName='Average rent'/>
-        <StatBox statName='Most expensive listing'/>
-        <StatBox statName='Cheapest listing'/>
+        <StatBox city={selectedCity} statName='Total listings'/>
+        <StatBox city={selectedCity} statName='Average rent'/>
+        <StatBox city={selectedCity} statName='Most expensive listing'/>
+        <StatBox city={selectedCity} statName='Cheapest listing'/>
         <BarGraph /> 
     </section>
   )
