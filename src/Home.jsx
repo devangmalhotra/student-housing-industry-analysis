d26d0197af91ec9717a4099ecfab628f68eaf1f5
devangmalhotra <malh2226@mylaurier.ca>
2025-04-30 23:40:55 -0400

    updated files

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,8 +3,31 @@ import Navbar from './Components/Navbar';
 import DashboardHeader from './Components/DashboardHeader';
 import MainContent from './Components/MainContent';
 import LineGraph from './Components/BarGraph';
+import { useState } from 'react';
 
 function Home() {
+  const [selectedCity, setSelectedCity] = useState('waterloo');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    getInfo();
+  }
+
+  const handleSelect = e => {
+      setSelectedCity(e.target.value);
+  }
+
+  // Gets the scraped info
+  async function getInfo() {
+      const res = await fetch(`http://localhost:8000/scrape?city=${selectedCity}`, 
+          {method: 'GET'}
+      )
+
+      console.log(res);
+      const data = await res.json();
+      console.log(data);
+  };
+
   return (
     <div id='home-main'>
         <Navbar />
